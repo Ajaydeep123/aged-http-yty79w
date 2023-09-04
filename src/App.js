80c790fa3c09c071139878d200b3b71d9bfbcd5f
CodeSandbox <support@codesandbox.io>
2023-09-04 13:43:06 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,97 @@
 import "./styles.css";
+import { useState } from "react";
+import usePasswordGenerator from "./hooks/usePasswordGenerator";
+import PasswordStrengthIndicator from "./components/StrengthChecker";
+import Button from "./components/Button";
+import Checkbox from "./components/Checkbox";
 
 export default function App() {
+  const [length, setLength] = useState(4);
+  const [checkboxData, setCheckboxData] = useState([
+    { title: "Include Uppercase Letters", state: false },
+    { title: "Include Lowercase Letters", state: false },
+    { title: "Include Numbers", state: false },
+    { title: "Include Symbols", state: false }
+  ]);
+  const [copied, setCopied] = useState(false);
+
+  const handleCheckboxChange = (i) => {
+    const updatedCheckboxData = [...checkboxData];
+    updatedCheckboxData[i].state = !updatedCheckboxData[i].state;
+    setCheckboxData(updatedCheckboxData);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const { password, errorMessage, generatePassword } = usePasswordGenerator();
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox v2</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      {/* Password Text and Copy */}
+      {password && (
+        <div className="header">
+          <div className="title">{password}</div>
+          <span>
+          <svg width="21" height="24" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"
+              fill="currentColor"
+            />
+          </svg>
+        </span>
+          {/* <Button
+            text={copied ? "Copied" : "copy"}
+            onClick={handleCopy}
+            customClass="copyBtn"
+          /> */}
+        </div>
+      )}
+      {/* Character Length */}
+      <div className="charlength">
+        <span >
+          <label>Character Length</label>
+          <label>{length}</label>
+        </span>
+        <input
+          type="range"
+          min="4"
+          max="20"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+        />
+      </div>
+      {/* Checkboxes */}
+      <div className="checkboxes">
+        {checkboxData.map((checkbox, index) => {
+          return (
+            <Checkbox
+              key={index}
+              title={checkbox.title}
+              onChange={() => handleCheckboxChange(index)}
+              state={checkbox.state}
+            />
+          );
+        })}
+      </div>
+      {/* Strength */}
+      <PasswordStrengthIndicator password={password} />
+
+      {/* Error Handling */}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+
+      {/* Generate Button */}
+      <Button
+        text="Generate Password"
+        onClick={() => generatePassword(checkboxData, length)}
+        customClass="generateBtn"
+      />
     </div>
   );
 }
